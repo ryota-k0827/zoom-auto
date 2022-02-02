e964f740c3c48f198c53ef6bc80b55da450e33fd
ryota-k0827 <ryota171124@icloud.com>
2022-02-02 19:17:11 +0900

    設定値を初期化

--- apprun.bat
+++ apprun.bat
@@ -1,11 +1,11 @@
 @REM ミーティングID
-set MEETINGID=
+set MEETINGID=1234567890
 @REM ミーティングパスワード
-set PASSWD=
+set PASSWD=password
 @REM 授業時間（分）
-set RECTIME=
+set RECTIME=90
 @REM チャットメッセージ（出席確認用）
-set MSG=
+set MSG=99999IH99A999山田太郎
 
 start zoommtg:"//zoom.us/join?action=join&confno=%MEETINGID%
 python ./app.py %PASSWD% %RECTIME% %MSG%
